caca5a5a6a2cdb7d06e9d6b30cb68aa51120eea9
Bilal Mohammed <bilalmd@umich.edu>
2026-07-03 11:30:02 -0400

    Update pressure_poisson_GS.m

--- Codes/pressure_poisson_GS.m
+++ Codes/pressure_poisson_GS.m
@@ -1,6 +1,6 @@
 function q = pressure_poisson_GS(Nx,del_x,del_y,RHS_P_pred)
 
-q = zeros(Nx+2,Nx+2);   % pressure correction with ghost cells
+q = zeros(Nx+2,Nx+2);
 
 Ax = del_y/del_x;
 Ay = del_x/del_y;
@@ -20,14 +20,14 @@ for iter = 1:max_iter
     q(:,1)     = q(:,2);       % left
     q(:,end)   = q(:,end-1);   % right
 
-    % Gauss-Seidel update for interior cells
+    % interior cells - GS
     for i = 2:Nx+1
         for j = 2:Nx+1
             q(i,j) = (Ax*(q(i,j+1) + q(i,j-1)) + Ay*(q(i-1,j) + q(i+1,j)) - RHS_P_pred(i-1,j-1) ) / denom;
         end
     end
 
-    % Fix pressure singularity
+    % pressure singular - fix
     q(2,2) = 0;
 
     % convergence check
